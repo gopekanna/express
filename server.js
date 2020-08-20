21601a251a2d2c275bc4c147490244b037150582
Gopi Kannan <gopekanna@gmail.com>
2020-08-20 21:04:11 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3000
 const config = require('./backend');
 const Routers = require('./Routers');
+const {GraphQ, schema} = require('./graph');
+const { Router } = require('express');
 
 mongoose.connect(config.DB, { useUnifiedTopology: true,useNewUrlParser: true }).then(
   () => {console.log('Database is connected') },
@@ -16,7 +18,11 @@ mongoose.connect(config.DB, { useUnifiedTopology: true,useNewUrlParser: true }).
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/user', Routers);
+app.use('/secur', Routers);
+app.use('/graphql', GraphQ({
+  schema: schema,
+  graphiql: true
+}));
 
 app.listen(PORT, () => {
   console.log('Server is running on PORT:',PORT);
